a94055c59b852d98dcba0d3c7fd35991e1c34b5c
CleuJunior <cleu.junior@gmail.com>
2023-07-28 10:04:22 -0300

    Sales by Match

---
+++ src/main/java/constant/FileWriterConstant.java
@@ -0,0 +1,6 @@
+package constant;
+
+public class FileWriterConstant {
+
+    public static String locale = "src/main/resources/output.txt";
+}
---
+++ src/main/java/warmup/SalesByMatch.java
@@ -0,0 +1,61 @@
+package warmup;
+
+import constant.FileWriterConstant;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class SalesByMatch {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        Map<Integer, Integer> sockCount = new HashMap<>();
+
+        for (int sock : ar) {
+            sockCount.put(sock, sockCount.getOrDefault(sock, 0) + 1);
+        }
+
+        int pairCount = 0;
+        for (int count : sockCount.values()) {
+            pairCount += count / 2;
+        }
+
+        return pairCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FileWriterConstant.locale));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = SalesByMatch.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
